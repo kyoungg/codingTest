3775284b98e6d708fda7d573c1ab51622396ae2e
Kyunghye Kim <123715126+kyoungg@users.noreply.github.com>
2026-03-03 18:58:10 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,16 @@
+function solution(num) {
+    let count = 0;
+    
+    while(num !== 1) {
+       if (num%2 === 0){
+        num = num/2
+        count+=1
+    } else {
+       num = num*3+1
+        count+=1
+    }
+}
+    
+return count <= 500 ? count :-1;
+    
+}
