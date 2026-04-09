e609c4d631786082d82fd54e436bf905fbbf4c97
Kyunghye Kim <123715126+kyoungg@users.noreply.github.com>
2026-04-09 16:34:09 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,19 @@
+function solution(a, b) {
+    var answer = 0;
+    
+    if(a%2 !== 0 ){
+        if(b%2 !== 0){
+            answer = a**2 + b**2
+        } else{
+            answer = 2*(a+b)
+        }
+    } else {
+        if(b%2 !== 0){
+            answer = 2*(a+b)
+        } else{
+            answer = Math.abs(a-b)
+        }
+    }
+    
+    return answer;
+}
